65b73338a929f3fc332a2420b91a9d816edd27f2
swallow0311 <774207663@qq.com>
2026-06-05 13:25:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
+  base: "/SmartCulturalRelic/",     // ← 新增这一行
   server: {
     host: "::",
     port: 8080,
